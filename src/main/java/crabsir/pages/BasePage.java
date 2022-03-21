0e0207f5c3caf2059f5eebc6b72241324a300c05
Crabsir <irbatir@gmail.com>
2022-03-21 18:19:02 +0100

    Added Wait methods

--- src/main/java/crabsir/pages/BasePage.java
+++ src/main/java/crabsir/pages/BasePage.java
@@ -1,7 +1,13 @@
 package crabsir.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
@@ -13,8 +19,23 @@ public class BasePage {
 
     public static BasePage goTo(String url) {
         driver = new ChromeDriver();
+        BasePage basePage = new BasePage(driver);
         driver.get(url);
-        return new BasePage(driver);
+        return basePage;
+    }
+
+    public static WebDriverWait webWait(WebDriver driver, long timeout) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+        wait.ignoring(NoSuchElementException.class);
+        return wait;
+    }
+
+    public static WebDriverWait webWait(WebDriver driver) {
+        return webWait(driver, 10);
+    }
+
+    public void ensurePageLoaded(String locator) {
+        webWait(driver).until(e -> e.findElement(By.cssSelector(locator)));
     }
 
 }
--- src/main/java/crabsir/pages/Locators.java
+++ src/main/java/crabsir/pages/Locators.java
@@ -1,4 +1,9 @@
 package crabsir.pages;
 
 public class Locators {
+
+    public static class SuperBirdCafePageLocators {
+        String ensureLocator = "a > img";
+    }
+
 }

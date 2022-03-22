f886a1ed722d8a897837c3cb9d87c161a41a44e4
Crabsir <irbatir@gmail.com>
2022-03-22 10:21:35 +0100

    Added selectSortingMethod

--- src/test/java/crabsir/pages/Locators.java
+++ src/test/java/crabsir/pages/Locators.java
@@ -8,6 +8,8 @@ public class Locators {
         public static String pageTitle = "British Airways Online Shop | Food and Drink On-board | " +
                 "British Airways Speedbird Café | Duty Free Goods Online | Buy On Board Online | BA Speedbird Café " +
                 "| BA High Life Shop Highlifeshop.com";
+
+        public static String sortingMethod = "#sorter";
     }
 
 }
--- src/test/java/crabsir/pages/SuperBirdCafePage.java
+++ src/test/java/crabsir/pages/SuperBirdCafePage.java
@@ -1,6 +1,8 @@
 package crabsir.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SuperBirdCafePage extends BasePage {
 
@@ -12,6 +14,13 @@ public class SuperBirdCafePage extends BasePage {
         return (SuperBirdCafePage) ensurePageLoaded(
                 Locators.SuperBirdCafePageLocators.ensureLocator, Locators.SuperBirdCafePageLocators.pageTitle
         );
+    }
+
+    public void selectSortingMethod(String method) {
+        Select sortingMethodDropdown = new Select (
+                driver.findElement(By.cssSelector(Locators.SuperBirdCafePageLocators.sortingMethod))
+        );
+        sortingMethodDropdown.selectByValue(method);
     }
 
 }

1347fc151d5a5edeae136ba8ad4c8674551bd3fc
Crabsir <irbatir@gmail.com>
2022-03-22 10:01:56 +0100

    Updated page title

--- src/test/java/crabsir/pages/Locators.java
+++ src/test/java/crabsir/pages/Locators.java
@@ -5,7 +5,9 @@ public class Locators {
     public static class SuperBirdCafePageLocators {
         public static String ensureLocator = "a > img";
 
-        public static String pageTitle = "";
+        public static String pageTitle = "British Airways Online Shop |  Food and Drink On-board | " +
+                "British Airways Speedbird Café | Duty Free Goods Online | Buy On Board Online | BA Speedbird Café " +
+                "| BA High Life Shop  Highlifeshop.com";
     }
 
 }
